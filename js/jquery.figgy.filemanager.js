f047d852f0a06b6824b45ed06a0cc0c4b59158f1
Shaun Ellis <shaun@sdellis.com>
2017-09-21 13:02:32 -0400

    multi-select working

--- js/jquery.figgy.filemanager.js
+++ js/jquery.figgy.filemanager.js
@@ -30,7 +30,7 @@ $.widget( "figgy.filemanager", {
             } else if (event.shiftKey) {
               console.log("shift")
             } else {
-              this.options.selected = [] // clear all selected
+              this.deselectAll()
               _this.handleSelectPage(event.target.id)
             }
   				}
@@ -44,8 +44,7 @@ $.widget( "figgy.filemanager", {
 
         this._on(this.document, {
           'input#label': function(event) {
-            this.options.selected.label = $( '#label' ).val()
-            window.selected = this.options.selected;
+            this.options.selected[0].label = $( '#label' ).val()
   				}
         });
 
@@ -77,9 +76,10 @@ $.widget( "figgy.filemanager", {
       this.element.append( $sidebar )
     },
     _createForm: function() {
-      var $form = $('<div class="form actions"></div>')
-
-      var markup = '<form id="page_metadata_form" class="form-horizontal">'+
+      var $form_panel = $('<div class="formPanel actions"></div>')
+      var $noneSelected = $('<p id="noneSelected" class="formContent">No items selected.</p>')
+      var $multiSelected = $('<p id="multiSelected" class="formContent">Multiple items are selected.</p>')
+      var singleSelected = '<form id="singleSelected" class="formContent form-horizontal">'+
                    '<div class="form-group">' +
                       '<label class="control-label" for="label">Label</label>' +
                       '<input type="text" name="label" id="label" value="1" class="form-control">' +
@@ -108,14 +108,25 @@ $.widget( "figgy.filemanager", {
                   '</div>' +
                   '<input id="canvas_id" type="hidden" name="canvas_id"></form>'
 
-      $form.append( markup )
-      this.element.append( $form )
+      $form_panel.append( $noneSelected, $multiSelected, singleSelected )
+      this.element.append( $form_panel )
     },
     _createControls: function() {
       var $controls = $('<div class="controls"></div>')
       var $button = $('<button id="save_btn" name="button" type="button" class="btn btn-primary">Save</button>')
       $controls.append( $button )
       this.element.append( $controls )
+    },
+    deselectAll: function() {
+      this.options.selected = []
+    },
+    _paintSelected: function() {
+      // loop through and add class to all the ids
+      $('.thumbnail img').removeClass('selected')
+      for (i = 0; i < this.options.selected.length; i++) {
+        var element = document.getElementById(this.options.selected[i].id)
+        element.className += " selected";
+      }
     },
     getImageById: function( id ) {
       var elementPos = this.options.images.map(function(image) {
@@ -124,14 +135,27 @@ $.widget( "figgy.filemanager", {
       return this.options.images[elementPos]
     },
     handleSelectPage: function( select_id ) {
-      var selected = this.getImageById(select_id)
-      $( '#label' ).val(selected.label)
-      $( '#pageType option[value="'+ selected.pageType +'"]' ).prop('selected', true)
-      $( '#isThumb' ).prop( "checked", selected.isThumb )
-      $( '#isStart' ).prop( "checked", selected.isStart )
-      $( '#canvas_id' ).val(selected.id)
-      $( '#detail_img' ).attr("src",selected.url)
-      this.options.selected.push(selected)
+      this.options.selected.push(this.getImageById(select_id))
+      $( '.formContent' ).hide()
+      switch (this.options.selected.length) {
+        case 0:
+          $( '#noneSelected' ).show()
+          break
+        case 1:
+          var selected = this.options.selected[0]
+          $( '#label' ).val(selected.label)
+          $( '#pageType option[value="'+ selected.pageType +'"]' ).prop('selected', true)
+          $( '#isThumb' ).prop( "checked", selected.isThumb )
+          $( '#isStart' ).prop( "checked", selected.isStart )
+          $( '#canvas_id' ).val(selected.id)
+          $( '#detail_img' ).attr("src",selected.url)
+          $( '#singleSelected' ).show()
+          break
+        default:
+          $( '#multiSelected' ).show()
+      }
+      console.log(this.options.selected)
+      this._paintSelected()
     },
     paintPage: function( page, index, array ) {
       $( "<div id='" + index + "' class='thumbnail'></div>" )
@@ -163,14 +187,17 @@ $.widget( "figgy.filemanager", {
       this._create();
     },
     save: function() {
+      for (i = 0; i < this.options.selected.length; i++) {
         var index = this.options.images.map(function(img) {
           return img.id;
-        }).indexOf(this.options.selected.id);
-        this.options.images[index] = this.options.selected
-        this.refresh()
-        //  This will save it to a server if we want
-        //  or we can just emit an event
-        this.element.trigger( "objectSaved", [this.options.images] );
+        }).indexOf(this.options.selected[i].id);
+        this.options.images[index] = this.options.selected[i]
+      }
+
+      this.refresh()
+      //  This could save it to a server if we want
+      //  but for now just emit an event
+      this.element.trigger( "objectSaved", [this.options.images] );
     },
     _destroy: function() {
         this.element
